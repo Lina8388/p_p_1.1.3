cc07ccaad24a3a6c8744f730513c2714a9e5d497
Lina8388 <v.sem93@mail.ru>
2021-12-17 01:48:39 +0300

    1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,23 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+
 public class Main {
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+
+        UserServiceImpl userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+        userService.saveUser("Дмитрий", "Морозов", (byte) 33);
+        userService.saveUser("Ирина", "Разумовская", (byte) 23);
+        userService.saveUser("Анна", "Литвинова", (byte) 38);
+        userService.saveUser("Иван", "Свиридов", (byte) 25);
+
+        System.out.println(userService.getAllUsers());
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,27 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    private static final String URL = "jdbc:mysql://localhost:3306/lesson_1_1_3?" + "useSSL=false"+"&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASS = "vfhbz638879";
+
+    public static Statement connect() throws SQLException {
+
+        Connection connection = DriverManager.getConnection(URL, USER, PASS);
+        Statement statement = connection.createStatement();
+
+
+          //  System.out.println("Соединение с БД установлено");
+            return statement;
+        // } catch (SQLException e) {
+           // System.err.println("Ошибка подключения к БД");
+        //}
+        //return null;
+    }
+
+
+
 }
